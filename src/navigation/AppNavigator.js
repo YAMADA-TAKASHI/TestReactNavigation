8db23d8d8c56fdd73e2f6a2461db11161ee8bdb9
YAMADA TAKASHI <yamadatakashi.work1203@gmail.com>
2019-10-25 08:40:56 +0900

    各画面を実装

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,38 @@
+import {
+    createStackNavigator,
+    createAppContainter,
+    createBottomTabNavigator,
+} from 'react-navigation';
+
+import StartScreen from "../screens/StartScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+//起動画面
+const Start = {
+    screen: StartScreen,
+    navigationOptions : ({ navigation }) => {
+        return {title: '起動画面',}
+    },
+}
+
+//ホーム画面
+const Home = {
+    screen : HomeScreen,
+    navigationOptions : ({ navigation }) => {
+        return {title: 'ホーム画面',};
+    },
+}
+
+const MainNavigation = createStackNavigator(
+    {
+        Start,
+        Home,
+    },
+    {
+        mode: 'card',
+        //headerMode: 'none',
+        initialRouteName: 'Start',  //最初に表示させる画面を設定
+    },
+);
+
+export default AppNavigator = createAppContainter(MainNavigation);
